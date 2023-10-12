734020b7a32a6b71b8bcbc9a2a061d86925f2be1
raulG91 <rgarciapedrosa@gmail.com>
2023-10-12 22:38:46 +0200

    SQLite

--- DB SQLite/set_db.py
+++ DB SQLite/set_db.py
@@ -6,6 +6,13 @@ cursor = conexion.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS usuarios " \
     "(nombre VARCHAR(100), edad INTEGER, email VARCHAR(100))")
 
+#cursor.execute("INSERT INTO usuarios VALUES ('Hector', 27, 'hector@ejemplo.com')")
+cursor.execute("SELECT * FROM usuarios")
+
+usuario = cursor.fetchone()
+print(usuario)
+
+
 conexion.commit()
 
 conexion.close()
